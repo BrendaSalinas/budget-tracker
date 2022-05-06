4a958baef478658a58891653383a513fb983c3d3
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-05-05 19:23:36 -0500

    Adding changes to the service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const indexedDB =
   window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB;
 
-const APP_PREFIX = "BudgetTracker-";
+const APP_PREFIX = "Budget-tracker";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -18,21 +18,17 @@ self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
+      if (request) {
         console.log('responding with cache : ' + e.request.url)
         return request
-      } else {       // if there are no cache, try fetching request
+      } else {     
         console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
     })
   )
 })
 
-// Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -42,16 +38,13 @@ self.addEventListener('install', function (e) {
   )
 })
 
-// Delete outdated caches
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create keeplist
+
       let cacheKeeplist = keyList.filter(function(key) {
         return key.indexOf(APP_PREFIX);
       });
-      // add current cache name to keeplist
       cacheKeeplist.push(CACHE_NAME);
 
       return Promise.all(
